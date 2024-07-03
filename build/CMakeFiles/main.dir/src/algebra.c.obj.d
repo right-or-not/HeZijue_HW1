462489e4d4add0fadda92b87758c9981616b9a12
HeZijue <jue1251665735@outlook.com>
2024-07-03 21:00:46 +0800

    Final Commit

---
+++ build/CMakeFiles/main.dir/src/algebra.c.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/main.dir/src/algebra.c.obj: \
+ D:\ZJU\TrainingCamp\01_BasicProgramming\HeZijue_HW1\src\algebra.c \
+ D:/ZJU/TrainingCamp/01_BasicProgramming/HeZijue_HW1/inc/algebra.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/stdio.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/math.h \
+ C:/TDM-GCC-64/x86_64-w64-mingw32/include/crtdefs.h
